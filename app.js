918c0d7b0ec6a1a2140055141684606ee3534b4d
James <jamesrea83@gmail.com>
2017-06-26 13:59:41 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//{"ipaddress":"94.142.173.221","language":"en-GB","software":"Windows NT 6.1"}
+
+var express = require("express"),
+    app = express();
+
+var result = {};    
+
+app.use(function(req, res) {
+    var agentString = req.headers["user-agent"];
+    
+    result.ipaddress = req.ip;
+    result.language = req.headers["accept-language"].slice(0, 5);
+    result.software = agentString.slice(agentString.indexOf("(") + 1, agentString.indexOf(")"));
+    
+    res.send(result);
+});
+
+app.listen(process.env.PORT);
