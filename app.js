1d14f7823607bf37bba7beecd2791d4703e29609
James <jamesrea83@gmail.com>
2017-06-28 08:32:25 +0000

    removed leading characters from ip string

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var result = {};
 app.use(function(req, res) {
     var agentString = req.headers["user-agent"];
     
-    result.ipaddress = req.ip;
+    result.ipaddress = req.ip.slice(7, req.ip.length);
     result.language = req.headers["accept-language"].slice(0, 5);
     result.software = agentString.slice(agentString.indexOf("(") + 1, agentString.indexOf(")"));
     
